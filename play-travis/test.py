b13752a228db30b652791bf2eb77a5c675aefdd2
Igor Beliakov <demtis.register@gmail.com>
2019-03-22 15:43:08 +0100

    Fixed test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,7 +3,7 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
 
 if __name__ == '__main__':
     unittest.main()
