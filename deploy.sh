44483dcc6e99f1085e5c4f1a94c7c154a2db6b3e
Igor Beliakov <demtis.register@gmail.com>
2019-03-29 11:58:03 +0100

    Deleted .sh files from main directory

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
-puma -d
-
--- install_ruby.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
-
-
